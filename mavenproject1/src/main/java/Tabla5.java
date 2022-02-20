76a03e4bca752d061f688348305fa5906e40736d
Layola27 <99640682+Layola27@users.noreply.github.com>
2022-02-20 15:55:53 +0100

    Tabla 5

---
+++ mavenproject1/src/main/java/Tabla5.java
@@ -0,0 +1,42 @@
+
+public class Tabla5 {
+
+    
+    public static void main(String[] args) {
+         int resultado =0; 
+        int tabla = 5;
+        int multiplicando = 0;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);
+        resultado += tabla;
+        multiplicando+=1;
+        System.out.println(tabla + " X " + multiplicando + " = " + resultado);  
+    }
+    
+}
